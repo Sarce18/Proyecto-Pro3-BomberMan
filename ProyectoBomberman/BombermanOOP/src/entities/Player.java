9ef650b40f4517185a6a291b5e781d34ab1d9f52
Gomes Austin, Sebastian Alexander <116978169+SebastianGomes03@users.noreply.github.com>
2023-11-08 00:22:56 -0400

    Add files via upload

---
+++ ProyectoBomberman/BombermanOOP/src/entities/Player.java
@@ -0,0 +1,173 @@
+package entities;
+
+import static main.Game.SCALE;
+import static utilz.Constants.PlayerConstants.*;
+import static utilz.Constants.Directions.*;
+import static utilz.HelpMethods.CanMoveHere;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import main.Game;
+import utilz.LoadSave;
+
+public class Player extends Entity {
+	private BufferedImage[][] animations;
+	private int aniTick, aniIndex, aniSpeed = 25;
+	private int playerAction = IDLE;
+	private boolean moving = false, attacking = false;
+	private boolean left, up, right, down;
+	private float playerSpeed = 1.0f * SCALE;
+	private int[][] lvlData;
+	private float xDrawOffset = 21 * Game.SCALE;
+	private float yDrawOffset = 4 * Game.SCALE;
+
+	public Player(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		loadAnimations();
+		initHitbox(x, y, 32 * Game.SCALE, 48 * Game.SCALE);
+
+	}
+
+	public void update() {
+		updatePos();
+		updateAnimationTick();
+		setAnimation();
+	}
+
+	public void render(Graphics g) {
+		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x), (int) (hitbox.y),(int)hitbox.width, (int)hitbox.height, null);
+		drawHitbox(g);
+	}
+
+	private void updateAnimationTick() {
+		aniTick++;
+		if (aniTick >= aniSpeed) {
+			aniTick = 0;
+			aniIndex++;
+			if (aniIndex >= GetSpriteAmount(playerAction)) {
+				aniIndex = 0;
+			}
+
+		}
+
+	}
+
+	private void setAnimation() {
+		int startAni = playerAction;
+
+		if (moving){
+			if(isLeft())
+				playerAction = LEFT;
+			else if(isRight())
+				playerAction = RIGHT;
+			else if(isUp())
+				playerAction = UP;
+			else if(isDown())
+				playerAction = 1;
+			//else
+			//playerAction = RUNNING;
+		}
+		else
+			playerAction = IDLE;
+
+		if (startAni != playerAction)
+			resetAniTick();
+	}
+
+	private void resetAniTick() {
+		aniTick = 0;
+		aniIndex = 0;
+	}
+
+	private void updatePos() {
+		moving = false;
+		if (!left && !right && !up && !down)
+			return;
+
+		float xSpeed = 0, ySpeed = 0;
+
+		if (left && !right)
+			xSpeed = -playerSpeed;
+		else if (right && !left)
+			xSpeed = playerSpeed;
+
+		if (up && !down)
+			ySpeed = -playerSpeed;
+		else if (down && !up)
+			ySpeed = playerSpeed;
+
+//		if (CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)) {
+//			this.x += xSpeed;
+//			this.y += ySpeed;
+//			moving = true;
+//		}
+
+		if (CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+			hitbox.x += xSpeed;
+			hitbox.y += ySpeed;
+			moving = true;
+		}
+
+	}
+
+	private void loadAnimations() {
+
+		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
+
+		//animations = new BufferedImage[9][6];
+		animations = new BufferedImage[4][3];
+		for (int j = 0; j < animations.length; j++)
+			for (int i = 0; i < animations[j].length; i++)
+				//animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
+				animations[j][i] = img.getSubimage(i * 32, j * 48, 32, 48);
+
+	}
+
+	public void loadLvlData(int[][] lvlData) {
+		this.lvlData = lvlData;
+	}
+
+	public void resetDirBooleans() {
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+	}
+
+	public void setAttacking(boolean attacking) {
+		this.attacking = attacking;
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isUp() {
+		return up;
+	}
+
+	public void setUp(boolean up) {
+		this.up = up;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
+	}
+
+	public boolean isDown() {
+		return down;
+	}
+
+	public void setDown(boolean down) {
+		this.down = down;
+	}
+
+}
---
+++ ProyectoBomberman/BombermanOOP/src/utilz/Constants.java
@@ -0,0 +1,55 @@
+package utilz;
+
+public class Constants {
+
+	public static class Directions {
+		public static final int UP = 0;
+		public static final int DOWN = 1;
+		public static final int LEFT = 2;
+		public static final int RIGHT = 3;
+	}
+
+	public static class PlayerConstants {
+		public static final int IDLE = 1;
+		//public static final int RUNNING = 3;
+		public static final int LEFTP = 2;
+		public static final int RIGHTP = 3;
+		public static final int UPP = 0;
+		//public static final int DOWN = 1;
+		//public static final int JUMP = 2;
+		//public static final int FALLING = 3;
+		//public static final int GROUND = 4;
+		//public static final int HIT = 5;
+		//public static final int ATTACK_1 = 6;
+		//public static final int ATTACK_JUMP_1 = 7;
+		//public static final int ATTACK_JUMP_2 = 8;
+
+		public static int GetSpriteAmount(int player_action) {
+			switch (player_action) {
+			
+			case LEFTP:
+				return 2;
+			case RIGHTP:
+				return 3;
+			case UPP:
+				return 0;
+			case IDLE:	
+				return 1;
+			//case HIT:
+				//return 4;
+			//case JUMP:
+			//case ATTACK_1:
+			//case ATTACK_JUMP_1:
+			//case ATTACK_JUMP_2:
+			//	return 3;
+			//case GROUND:
+			//	return 2;
+			//case FALLING:
+			default:
+				return 1;
+			}
+		}
+
+
+	}
+}
